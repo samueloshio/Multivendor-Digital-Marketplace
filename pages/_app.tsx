c8cc78d693711e7d9977d91d06b89b472ca78714
Samuel Oshio <samueloshio5@gmail.com>
2023-08-17 03:25:52 +0100

    Project, Folder Structure, & Prisma Initialized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@/components/Theme/ThemeProvider"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <Component {...pageProps} />
+  </ThemeProvider>)
 }
